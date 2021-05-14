45f08b22ff339236e678c16c996fa2f0b5c27e00
StockMaker Company <83646373+stockmakercompany@users.noreply.github.com>
2021-05-14 12:45:48 +0200

    Update stockmaker_inv.sql

--- stockmaker_inv.sql
+++ stockmaker_inv.sql
@@ -1,7 +1,15 @@
-
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-06-2017 a las 07:12:59
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 7.0.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 
 
@@ -10,11 +18,18 @@ CREATE TABLE `categories` (
   `name` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+--
+-- Volcado de datos para la tabla `categories`
+--
 
 INSERT INTO `categories` (`id`, `name`) VALUES
 (1, 'Repuestos');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `media`
+--
 
 CREATE TABLE `media` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -22,12 +37,18 @@ CREATE TABLE `media` (
   `file_type` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `media`
+--
 
 INSERT INTO `media` (`id`, `file_name`, `file_type`) VALUES
 (1, 'filter.jpg', 'image/jpeg');
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `products`
+--
 
 CREATE TABLE `products` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -40,23 +61,32 @@ CREATE TABLE `products` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `products`
+--
 
 INSERT INTO `products` (`id`, `name`, `quantity`, `buy_price`, `sale_price`, `categorie_id`, `media_id`, `date`) VALUES
 (1, 'Filtro de gasolina', '100', '5.00', '10.00', 1, 1, '2017-06-16 07:03:16');
 
+-- --------------------------------------------------------
 
-
-
+--
+-- Estructura de tabla para la tabla `sales`
+--
 
 CREATE TABLE `sales` (
   `id` int(11) UNSIGNED NOT NULL,
   `product_id` int(11) UNSIGNED NOT NULL,
-  `qty` int(11) NOT 
+  `qty` int(11) NOT NULL,
   `price` decimal(25,2) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -69,14 +99,20 @@ CREATE TABLE `users` (
   `last_login` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `users`
+--
 
 INSERT INTO `users` (`id`, `name`, `username`, `password`, `user_level`, `image`, `status`, `last_login`) VALUES
 (1, 'Admin Users', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1, 'pzg9wa7o1.jpg', 1, '2017-06-16 07:11:11'),
 (2, 'Special User', 'special', 'ba36b97a41e7faf742ab09bf88405ac04f99599a', 2, 'no_image.jpg', 1, '2017-06-16 07:11:26'),
 (3, 'Default User', 'user', '12dea96fec20593566ab75692c9949596833adc9', 3, 'no_image.jpg', 1, '2017-06-16 07:11:03');
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `user_groups`
+--
 
 CREATE TABLE `user_groups` (
   `id` int(11) NOT NULL,
@@ -85,73 +121,120 @@ CREATE TABLE `user_groups` (
   `group_status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Volcado de datos para la tabla `user_groups`
+--
 
 INSERT INTO `user_groups` (`id`, `group_name`, `group_level`, `group_status`) VALUES
 (1, 'Admin', 1, 1),
 (2, 'Special', 2, 0),
 (3, 'User', 3, 1);
 
+--
+-- Índices para tablas volcadas
+--
 
+--
+-- Indices de la tabla `categories`
+--
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`);
 
-
+--
+-- Indices de la tabla `media`
+--
 ALTER TABLE `media`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id` (`id`);
 
-
+--
+-- Indices de la tabla `products`
+--
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name` (`name`),
   ADD KEY `categorie_id` (`categorie_id`),
   ADD KEY `media_id` (`media_id`);
 
-
+--
+-- Indices de la tabla `sales`
+--
 ALTER TABLE `sales`
   ADD PRIMARY KEY (`id`),
   ADD KEY `product_id` (`product_id`);
 
-
+--
+-- Indices de la tabla `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD KEY `user_level` (`user_level`);
 
-
+--
+-- Indices de la tabla `user_groups`
+--
 ALTER TABLE `user_groups`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `group_level` (`group_level`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
 ALTER TABLE `categories`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+--
+-- AUTO_INCREMENT de la tabla `media`
+--
 ALTER TABLE `media`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
 ALTER TABLE `products`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
+--
+-- AUTO_INCREMENT de la tabla `sales`
+--
 ALTER TABLE `sales`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
 ALTER TABLE `users`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
+--
+-- AUTO_INCREMENT de la tabla `user_groups`
+--
 ALTER TABLE `user_groups`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `products`
+--
 ALTER TABLE `products`
   ADD CONSTRAINT `FK_products` FOREIGN KEY (`categorie_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
+--
+-- Filtros para la tabla `sales`
+--
 ALTER TABLE `sales`
   ADD CONSTRAINT `SK` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-
+--
+-- Filtros para la tabla `users`
+--
 ALTER TABLE `users`
   ADD CONSTRAINT `FK_user` FOREIGN KEY (`user_level`) REFERENCES `user_groups` (`group_level`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
