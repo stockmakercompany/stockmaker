5a04ac18f39e7ed41d38dc74faa178f6464f7cf8
StockMaker Company <83646373+stockmakercompany@users.noreply.github.com>
2021-05-18 21:44:36 +0200

    Add files via upload

---
+++ layouts/header.php
@@ -0,0 +1,73 @@
+<?php $user = current_user(); ?>
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title><?php if (!empty($page_title))
+           echo remove_junk($page_title);
+            elseif(!empty($user))
+           echo ucfirst($user['name']);
+            else echo "Sistema de Inventario";?>
+    </title>
+	
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker3.min.css" />
+    <link rel="stylesheet" href="libs/css/main.css" />
+    <style>
+      #logo-mod {
+        width: 25%;
+      }
+    </style>
+ 
+  </head>
+  <body>
+  <?php  if ($session->isUserLoggedIn(true)): ?>
+    <header id="header">
+      <div class="logo pull-left" > 
+      <a id="menus"href="home.php" title="Menu">
+                     <i class="glyphicon glyphicon-home" st></i>
+                      Menu</a>  </div>
+      <div class="header-content">
+      <div class="header-date pull-left">
+
+      </div>
+
+      <div class="pull-right clearfix">
+        <ul class="info-menu list-inline list-unstyled">
+          <li class="profile">
+            <a href="#" data-toggle="dropdown" class="toggle" aria-expanded="false">
+              <img src="uploads/users/<?php echo $user['image'];?>" alt="user-image" class="img-circle img-inline">
+              <span><?php echo remove_junk(ucfirst($user['name'])); ?> <i class="caret"></i></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                  <a href="profile.php?id=<?php echo (int)$user['id'];?>">
+                      <i class="glyphicon glyphicon-cog"></i>
+                      Perfil
+                  </a>
+              </li>
+              <li>
+                  <a href="users.php?id=<?php echo (int)$user['id'];?>">
+                      <i class="glyphicon glyphicon-user"></i>
+                      Usuarios
+                  </a>
+              </li>
+             
+             <li class="last">
+                 <a href="logout.php">
+                     <i class="glyphicon glyphicon-off"></i>
+                     Salir
+                 </a>
+             </li>
+           </ul>
+          </li>
+        </ul>
+      </div>
+     </div>
+    </header>
+    
+
+<?php endif;?>
+
+<div class="page">
+  <div class="container-fluid">
