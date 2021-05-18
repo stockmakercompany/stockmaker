237af35bba9e3cfd6ee1e2a3f6f5a0cf9b8fa764
StockMaker Company <83646373+stockmakercompany@users.noreply.github.com>
2021-05-18 21:42:05 +0200

    Add files via upload

---
+++ includes/sql.php
@@ -0,0 +1,230 @@
+<?php
+  require_once('includes/load.php');
+
+
+function find_all($table) {
+   global $db;
+   if(tableExists($table))
+   {
+     return find_by_sql("SELECT * FROM ".$db->escape($table));
+   }
+}
+
+function find_by_sql($sql)
+{
+  global $db;
+  $result = $db->query($sql);
+  $result_set = $db->while_loop($result);
+ return $result_set;
+}
+
+function find_by_id($table,$id)
+{
+  global $db;
+  $id = (int)$id;
+    if(tableExists($table)){
+          $sql = $db->query("SELECT * FROM {$db->escape($table)} WHERE id='{$db->escape($id)}' LIMIT 1");
+          if($result = $db->fetch_assoc($sql))
+            return $result;
+          else
+            return null;
+     }
+}
+
+function delete_by_id($table,$id)
+{
+  global $db;
+  if(tableExists($table))
+   {
+    $sql = "DELETE FROM ".$db->escape($table);
+    $sql .= " WHERE id=". $db->escape($id);
+    $sql .= " LIMIT 1";
+    $db->query($sql);
+    return ($db->affected_rows() === 1) ? true : false;
+   }
+}
+
+function count_by_id($table){
+  global $db;
+  if(tableExists($table))
+  {
+    $sql    = "SELECT COUNT(id) AS total FROM ".$db->escape($table);
+    $result = $db->query($sql);
+     return($db->fetch_assoc($result));
+  }
+}
+
+function tableExists($table){
+  global $db;
+  $table_exit = $db->query('SHOW TABLES FROM '.DB_NAME.' LIKE "'.$db->escape($table).'"');
+      if($table_exit) {
+        if($db->num_rows($table_exit) > 0)
+              return true;
+         else
+              return false;
+      }
+  }
+
+  function authenticate($username='', $password='') {
+    global $db;
+    $username = $db->escape($username);
+    $password = $db->escape($password);
+    $sql  = sprintf("SELECT id,username,password,user_level FROM users WHERE username ='%s' LIMIT 1", $username);
+    $result = $db->query($sql);
+    if($db->num_rows($result)){
+      $user = $db->fetch_assoc($result);
+      $password_request = sha1($password);
+      if($password_request === $user['password'] ){
+        return $user['id'];
+      }
+    }
+   return false;
+  }
+
+   function authenticate_v2($username='', $password='') {
+     global $db;
+     $username = $db->escape($username);
+     $password = $db->escape($password);
+     $sql  = sprintf("SELECT id,username,password,user_level FROM users WHERE username ='%s' LIMIT 1", $username);
+     $result = $db->query($sql);
+     if($db->num_rows($result)){
+       $user = $db->fetch_assoc($result);
+       $password_request = sha1($password);
+       if($password_request === $user['password'] ){
+         return $user;
+       }
+     }
+    return false;
+   }
+
+
+  function current_user(){
+      static $current_user;
+      global $db;
+      if(!$current_user){
+         if(isset($_SESSION['user_id'])):
+             $user_id = intval($_SESSION['user_id']);
+             $current_user = find_by_id('users',$user_id);
+        endif;
+      }
+    return $current_user;
+  }
+
+  function find_all_user(){
+      global $db;
+      $results = array();
+      $sql = "SELECT u.id,u.name,u.username,u.user_level,u.status,u.last_login,";
+      $sql .="g.group_name ";
+      $sql .="FROM users u ";
+      $sql .="LEFT JOIN user_groups g ";
+      $sql .="ON g.group_level=u.user_level ORDER BY u.name ASC";
+      $result = find_by_sql($sql);
+      return $result;
+  }
+
+
+ function updateLastLogIn($user_id)
+	{
+		global $db;
+    $date = make_date();
+    $sql = "UPDATE users SET last_login='{$date}' WHERE id ='{$user_id}' LIMIT 1";
+    $result = $db->query($sql);
+    return ($result && $db->affected_rows() === 1 ? true : false);
+	}
+
+
+  function find_by_groupName($val)
+  {
+    global $db;
+    $sql = "SELECT group_name FROM user_groups WHERE group_name = '{$db->escape($val)}' LIMIT 1 ";
+    $result = $db->query($sql);
+    return($db->num_rows($result) === 0 ? true : false);
+  }
+
+  function find_by_groupLevel($level)
+  {
+    global $db;
+    $sql = "SELECT group_level FROM user_groups WHERE group_level = '{$db->escape($level)}' LIMIT 1 ";
+    $result = $db->query($sql);
+    return($db->num_rows($result) === 0 ? true : false);
+  }
+
+   function page_require_level($require_level){
+     global $session;
+     $current_user = current_user();
+     $login_level = find_by_groupLevel($current_user['user_level']);
+
+     if (!$session->isUserLoggedIn(true)):
+            $session->msg('d','Por favor Iniciar sesión...');
+            redirect('index.php', false);
+
+     elseif($current_user['user_level'] <= (int)$require_level):
+              return true;
+      else:
+            $session->msg("d", "¡Lo siento!  no tienes permiso para ver la página.");
+            redirect('home.php', false);
+        endif;
+
+     }
+
+  function join_product_table(){
+     global $db;
+     $sql  =" SELECT p.id,p.name,p.quantity,p.buy_price,p.sale_price,p.media_id,p.date,c.name";
+    $sql  .=" AS categorie,m.file_name AS image";
+    $sql  .=" FROM products p";
+    $sql  .=" LEFT JOIN categories c ON c.id = p.categorie_id";
+    $sql  .=" LEFT JOIN media m ON m.id = p.media_id";
+    $sql  .=" ORDER BY p.id ASC";
+    return find_by_sql($sql);
+
+   }
+
+   function join_product_tablealert(){
+    global $db;
+    $sql  =" SELECT p.id,p.name,p.quantity,p.buy_price,p.sale_price,p.media_id,p.date,c.name";
+   $sql  .=" AS categorie,m.file_name AS image";
+   $sql  .=" FROM products p";
+   $sql  .=" LEFT JOIN categories c ON c.id = p.categorie_id";
+   $sql  .=" LEFT JOIN media m ON m.id = p.media_id";
+   $sql  .=" WHERE (p.minquantity >= p.quantity)";
+   $sql  .=" ORDER BY p.id ASC";
+   return find_by_sql($sql);
+  }
+
+
+
+
+   function find_product_by_title($product_name){
+    global $db;
+    $p_name = remove_junk($db->escape($product_name));
+    $sql = "SELECT name FROM products WHERE name like '%$p_name%' LIMIT 5";
+    $result = find_by_sql($sql);
+    return $result;
+  }
+
+
+  function find_all_product_info_by_title($title){
+    global $db;
+    $sql  = "SELECT * FROM products ";
+    $sql .= " WHERE name ='{$title}'";
+    $sql .=" LIMIT 1";
+    return find_by_sql($sql);
+  }
+
+  
+  function update_product_qty($qty,$p_id){
+    global $db;
+    $qty = (int) $qty;
+    $id  = (int)$p_id;
+    $sql = "UPDATE products SET quantity=quantity -'{$qty}' WHERE id = '{$id}'";
+    $result = $db->query($sql);
+    return($db->affected_rows() === 1 ? true : false);
+
+  }
+  
+
+?>
+
+
+          
+            
